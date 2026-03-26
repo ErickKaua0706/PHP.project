7ab4e9c61bd6b81996b26f996cc9ff10acaff6d1
ErickKaua <erickkaua0706@gmail.com>
2026-03-26 15:19:32 -0300

    teste de comitt

---
+++ PHP.project/index.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href=".css">
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Project</title>
+</head>
+<body>
+    <?php 
+    
+    echo "Hello World!";
+    var_dump("Hello World!"
+    );
+    $name = "Erick";
+    echo "meu nome e $name";
+    
+    
+    
+    ?>
+</body>
+</html>
